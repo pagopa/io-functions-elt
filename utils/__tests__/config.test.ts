c59ab8917f99b63a79832cf1664ed99dd32251f2
fabriziopapi <80677434+fabriziopapi@users.noreply.github.com>
2021-10-14 17:37:35 +0200

    [#IP-442]  Integration with PDND for Services cosmos entity (#5)
    * (+) add cdc for services cosmos container
    
    * (+) add kafka producer compact to manage broker/topic config as single block
    
    * (+) add store errors via storage table
    
    * (+) fix stored errors partition key
    
    * (+) add fp-ts-kafkajs impl

---
+++ utils/__tests__/config.test.ts
@@ -0,0 +1,69 @@
+import {
+  nestifyPrefixedType,
+  ValidableKafkaProducerConfigFromEnv
+} from "../config";
+import * as E from "fp-ts/Either";
+
+const dummyEnv = {
+  OTHERS: "others env properties",
+  TARGETKAFKA_clientId: "IO_FUNCTIONS_ELT",
+  TARGETKAFKA_brokers: "localhost:9093",
+  TARGETKAFKA_ssl: "true",
+  TARGETKAFKA_sasl_mechanism: "plain",
+  TARGETKAFKA_sasl_username: "username",
+  TARGETKAFKA_sasl_password: "password",
+  TARGETKAFKA_maxInFlightRequests: "1",
+  TARGETKAFKA_idempotent: "true",
+  TARGETKAFKA_transactionalId: "IO_ELT"
+};
+
+const dummyNestedEnv = {
+  brokers: "localhost:9093",
+  clientId: "IO_FUNCTIONS_ELT",
+  idempotent: "true",
+  maxInFlightRequests: "1",
+  sasl: {
+    mechanism: "plain",
+    password: "password",
+    username: "username"
+  },
+  ssl: "true",
+  transactionalId: "IO_ELT"
+};
+
+const dummyTargetKafkaConfig = {
+  brokers: ["localhost:9093"],
+  clientId: "IO_FUNCTIONS_ELT",
+  idempotent: true,
+  maxInFlightRequests: 1,
+  sasl: {
+    mechanism: "plain",
+    password: "password",
+    username: "username"
+  },
+  ssl: true,
+  transactionalId: "IO_ELT"
+};
+
+describe("config", () => {
+  it("GIVEN an env containing also properties in the format TARGETKAFKA_<field_path> WHEN nestifyPrefixedType is called THEN an object with <field_path> as nested field is returned", () => {
+    const processed = nestifyPrefixedType(dummyEnv, "TARGETKAFKA");
+    expect(processed).toStrictEqual(dummyNestedEnv);
+  });
+
+  it("GIVEN a not valid kafka producer configuration WHEN the decode is called THEN a left either is returned", () => {
+    const aaa = ValidableKafkaProducerConfigFromEnv.decode({
+      ...dummyEnv,
+      TARGETKAFKA_clientId: 1
+    });
+    expect(E.isLeft(aaa)).toBeTruthy();
+  });
+
+  it("GIVEN a valid kafka producer configuration WHEN the decode is called THEN a right either is returned", () => {
+    const aaa = ValidableKafkaProducerConfigFromEnv.decode(dummyEnv);
+    expect(E.isRight(aaa)).toBeTruthy();
+    if (E.isRight(aaa)) {
+      expect(E.getOrElseW(() => "")(aaa)).toStrictEqual(dummyTargetKafkaConfig);
+    }
+  });
+});
--- utils/config.ts
+++ utils/config.ts
@@ -4,36 +4,134 @@
  * Single point of access for the application confguration. Handles validation on required environment variables.
  * The configuration is evaluate eagerly at the first access to the module. The module exposes convenient methods to access such value.
  */
-
 import * as t from "io-ts";
-
-import * as E from "fp-ts/lib/Either";
+import * as E from "fp-ts/Either";
 import { pipe } from "fp-ts/lib/function";
-
 import { readableReport } from "@pagopa/ts-commons/lib/reporters";
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import * as R from "fp-ts/Record";
+import * as S from "fp-ts/string";
+import { set } from "lodash";
+
+import { KafkaProducerCompactConfig } from "./IoKafkaTypes";
+
+const isRecordOfString = (i: unknown): i is Record<string, unknown> =>
+  typeof i === "object" &&
+  i !== null &&
+  !Object.keys(i).some(property => typeof property !== "string");
+
+const createNotRecordOfStringErrorL = (
+  input: unknown,
+  context: t.Context
+) => (): t.Errors => [
+  {
+    context,
+    message: "input is not a valid record of string",
+    value: input
+  }
+];
+
+/**
+ * This functions create an object containig only the properties starting with 'prefix'. The env properties name will be splited using '_' to create nested object.
+ * eg. TARGETKAFKA_client_id: 1234 => { client: { id: 1234 } }
+ *
+ * @param env the input env
+ * @param prefix the properties prefix
+ * @returns a object
+ */
+export const nestifyPrefixedType = (
+  env: Record<string, unknown>,
+  prefix: string
+): Record<string, unknown> =>
+  pipe(
+    env,
+    R.filterWithIndex(fieldName => fieldName.split("_")[0] === prefix),
+    R.reduceWithIndex(S.Ord)({}, (k, b, a) =>
+      set(
+        b,
+        // eslint-disable-next-line functional/immutable-data
+        k
+          .split("_")
+          .splice(1)
+          .join("."),
+        a
+      )
+    )
+  );
+
+export type KafkaProducerCompactConfig = t.TypeOf<
+  typeof KafkaProducerCompactConfig
+>;
+export const KafkaProducerCompactConfigFromEnv = new t.Type<
+  KafkaProducerCompactConfig,
+  KafkaProducerCompactConfig,
+  unknown
+>(
+  "KafkaProducerCompactConfigFromEnv",
+  (u: unknown): u is KafkaProducerCompactConfig =>
+    KafkaProducerCompactConfig.is(u),
+  (input, context) =>
+    pipe(
+      input,
+      E.fromPredicate(
+        isRecordOfString,
+        createNotRecordOfStringErrorL(input, context)
+      ),
+      E.chainW(inputRecord =>
+        KafkaProducerCompactConfig.validate(
+          nestifyPrefixedType(inputRecord, "TARGETKAFKA"),
+          context
+        )
+      )
+    ),
+  t.identity
+);
 
 // global app configuration
-export type IConfig = t.TypeOf<typeof IConfig>;
-// eslint-disable-next-line @typescript-eslint/ban-types
-export const IConfig = t.interface({
-  AzureWebJobsStorage: NonEmptyString,
-
+export type IDecodableConfig = t.TypeOf<typeof IDecodableConfig>;
+export const IDecodableConfig = t.interface({
   COSMOSDB_KEY: NonEmptyString,
   COSMOSDB_NAME: NonEmptyString,
   COSMOSDB_URI: NonEmptyString,
 
-  QueueStorageConnection: NonEmptyString,
+  ERROR_STORAGE_ACCOUNT: NonEmptyString,
+  ERROR_STORAGE_KEY: NonEmptyString,
+  ERROR_STORAGE_TABLE: NonEmptyString,
 
   isProduction: t.boolean
 });
+
+export interface IParsableConfig {
+  readonly targetKafka: KafkaProducerCompactConfig;
+  // readonly servicesTopic: KafkaProducerTopicConfig;
+}
+
+export const parseConfig = (input: unknown): t.Validation<IParsableConfig> =>
+  pipe(
+    E.Do,
+    E.bind("targetKafka", () => KafkaProducerCompactConfigFromEnv.decode(input))
+    // E.bind("servicesTopic", () => KafkaProducerTopicConfigFromEnv.decode(input))
+  );
+
+export type IConfig = IDecodableConfig & IParsableConfig;
+export const IConfig = new t.Type<IConfig>(
+  "IConfig",
+  (u: unknown): u is IConfig => IDecodableConfig.is(u),
+  (input, context) =>
+    pipe(
+      E.Do,
+      E.bind("dc", () => IDecodableConfig.validate(input, context)),
+      E.bind("pc", () => parseConfig(input)),
+      E.map(({ dc, pc }) => ({ ...dc, ...pc }))
+    ),
+  t.identity
+);
 
 export const envConfig = {
   ...process.env,
   isProduction: process.env.NODE_ENV === "production"
 };
 
-// No need to re-evaluate this object for each call
 const errorOrConfig: t.Validation<IConfig> = IConfig.decode(envConfig);
 
 /**
