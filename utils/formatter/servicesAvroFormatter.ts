f1e5f7a5da358e17724557764506054812ef8e55
gquadrati <greta.quadrati@pagopa.it>
2021-12-20 10:42:18 +0100

    [#IC-154] update service avro field

--- utils/formatter/servicesAvroFormatter.ts
+++ utils/formatter/servicesAvroFormatter.ts
@@ -1,41 +1,73 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-import { RetrievedService } from "@pagopa/io-functions-commons/dist/src/models/service";
+import {
+  RetrievedService,
+  ValidService
+} from "@pagopa/io-functions-commons/dist/src/models/service";
 import * as avro from "avsc";
 import { MessageFormatter } from "../kafka/KafkaTypes";
 import { services } from "../../generated/avro/dto/services";
 import { CrudOperation } from "../../generated/avro/dto/CrudOperationEnum";
 
-export const avroServiceFormatter: MessageFormatter<RetrievedService> = message => ({
+export const buildAvroServiceObject = (
+  retrievedService: RetrievedService,
+  SERVICEID_EXCLUSION_LIST: ReadonlyArray<string>
+): Omit<services, "schema" | "subject"> => {
+  // eslint-disable-next-line no-console, no-underscore-dangle
+  console.log("TIMESTAMP", retrievedService._ts);
+  return {
+    departmentName: retrievedService.departmentName,
+    id: retrievedService.id,
+    isVisible: retrievedService.isVisible,
+    maxAllowedPaymentAmount: retrievedService.maxAllowedPaymentAmount,
+    metadata_address: retrievedService?.serviceMetadata?.address ?? "",
+    metadata_appAndroid: retrievedService?.serviceMetadata?.appAndroid ?? "",
+    metadata_appIos: retrievedService?.serviceMetadata?.appIos ?? "",
+    metadata_cta: retrievedService?.serviceMetadata?.cta ?? "",
+    metadata_description: retrievedService?.serviceMetadata?.description ?? "",
+    metadata_email: retrievedService?.serviceMetadata?.email ?? "",
+    metadata_pec: retrievedService?.serviceMetadata?.pec ?? "",
+    metadata_phone: retrievedService?.serviceMetadata?.phone ?? "",
+    metadata_privacyUrl: retrievedService?.serviceMetadata?.privacyUrl ?? "",
+    metadata_scope: retrievedService?.serviceMetadata?.scope ?? "",
+    metadata_supportUrl: retrievedService?.serviceMetadata?.supportUrl ?? "",
+    metadata_tokenName: retrievedService?.serviceMetadata?.tokenName ?? "",
+    metadata_tosUrl: retrievedService?.serviceMetadata?.tosUrl ?? "",
+    metadata_webUrl: retrievedService?.serviceMetadata?.webUrl ?? "",
+    op:
+      retrievedService.version === 0
+        ? CrudOperation.CREATE
+        : CrudOperation.UPDATE,
+    organizationFiscalCode: retrievedService.organizationFiscalCode,
+    organizationName: retrievedService.organizationName,
+    requireSecureChannels: retrievedService.requireSecureChannels,
+    serviceId: retrievedService.serviceId,
+    serviceName: retrievedService.serviceName,
+    version: retrievedService.version,
+    // eslint-disable-next-line sort-keys, no-underscore-dangle
+    timestamp: retrievedService._ts,
+
+    // --------------------
+    // Calculated values
+    // --------------------
+    // eslint-disable-next-line sort-keys
+    isQuality:
+      SERVICEID_EXCLUSION_LIST.indexOf(retrievedService.serviceId) > -1
+        ? true
+        : ValidService.is(retrievedService)
+  };
+};
+
+export const avroServiceFormatter = (
+  SERVICEID_EXCLUSION_LIST: ReadonlyArray<string>
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+): MessageFormatter<RetrievedService> => message => ({
   key: message.serviceId,
   value: avro.Type.forSchema(
     services.schema as avro.Schema // cast due to tsc can not proper recognize object as avro.Schema (eg. if you use const schemaServices: avro.Type = JSON.parse(JSON.stringify(services.schema())); it will loose the object type and it will work fine)
   ).toBuffer(
-    Object.assign(new services(), {
-      departmentName: message.departmentName,
-      id: message.id,
-      isVisible: message.isVisible,
-      maxAllowedPaymentAmount: message.maxAllowedPaymentAmount,
-      metadata_address: message?.serviceMetadata?.address ?? "",
-      metadata_appAndroid: message?.serviceMetadata?.appAndroid ?? "",
-      metadata_appIos: message?.serviceMetadata?.appIos ?? "",
-      metadata_cta: message?.serviceMetadata?.cta ?? "",
-      metadata_description: message?.serviceMetadata?.description ?? "",
-      metadata_email: message?.serviceMetadata?.email ?? "",
-      metadata_pec: message?.serviceMetadata?.pec ?? "",
-      metadata_phone: message?.serviceMetadata?.phone ?? "",
-      metadata_privacyUrl: message?.serviceMetadata?.privacyUrl ?? "",
-      metadata_scope: message?.serviceMetadata?.scope ?? "",
-      metadata_supportUrl: message?.serviceMetadata?.supportUrl ?? "",
-      metadata_tokenName: message?.serviceMetadata?.tokenName ?? "",
-      metadata_tosUrl: message?.serviceMetadata?.tosUrl ?? "",
-      metadata_webUrl: message?.serviceMetadata?.webUrl ?? "",
-      op: message.version === 0 ? CrudOperation.CREATE : CrudOperation.UPDATE,
-      organizationFiscalCode: message.organizationFiscalCode,
-      organizationName: message.organizationName,
-      requireSecureChannels: message.requireSecureChannels,
-      serviceId: message.serviceId,
-      serviceName: message.serviceName,
-      version: message.version
-    })
+    Object.assign(
+      new services(),
+      buildAvroServiceObject(message, SERVICEID_EXCLUSION_LIST)
+    )
   )
 });
