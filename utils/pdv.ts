f8adcee3b80861083c104a6f18f08b991eace7bd
gquadrati <greta.quadrati@pagopa.it>
2024-07-25 18:15:42 +0200

    [#IOPID-1909] add mocked PDV Id retriever

---
+++ utils/pdv.ts
@@ -0,0 +1,17 @@
+import * as crypto from "crypto";
+import * as TE from "fp-ts/TaskEither";
+
+import { FiscalCode, NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+
+// TODO: Remove as soon as PDV service is called
+const sha256 = (s: string): NonEmptyString =>
+  crypto
+    .createHash("sha256")
+    .update(s)
+    .digest("hex") as NonEmptyString;
+
+export const getPdvId: (
+  fiscalCode: FiscalCode
+) => TE.TaskEither<Error, NonEmptyString> = fiscalCode =>
+  // Return the hash of the fiscal code, as mocked PDV Id
+  TE.of(sha256(fiscalCode));
