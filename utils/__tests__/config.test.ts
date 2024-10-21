35564664c6af6878bb74114ffdcb8cbd3817fa03
Daniele Manni <dan.manni@gmail.com>
2024-10-21 09:33:21 +0200

    [#IOPID-2345] unit test config without SASL parameters

--- utils/__tests__/config.test.ts
+++ utils/__tests__/config.test.ts
@@ -4,18 +4,22 @@ import {
 } from "../config";
 import * as E from "fp-ts/Either";
 
-const dummyEnv = {
+const dummyEnvWithoutSASL = {
   OTHERS: "others env properties",
   TARGETKAFKA_clientId: "IO_FUNCTIONS_ELT",
   TARGETKAFKA_brokers: "localhost:9093",
   TARGETKAFKA_ssl: "true",
-  TARGETKAFKA_sasl_mechanism: "plain",
-  TARGETKAFKA_sasl_username: "username",
-  TARGETKAFKA_sasl_password: "password",
   TARGETKAFKA_maxInFlightRequests: "1",
   TARGETKAFKA_idempotent: "true",
   TARGETKAFKA_transactionalId: "IO_ELT",
   TARGETKAFKA_topic: "io-cosmosdb-services"
+};
+
+const dummyEnv = {
+  ...dummyEnvWithoutSASL,
+  TARGETKAFKA_sasl_mechanism: "plain",
+  TARGETKAFKA_sasl_username: "username",
+  TARGETKAFKA_sasl_password: "password"
 };
 
 const dummyNestedEnv = {
@@ -33,19 +37,23 @@ const dummyNestedEnv = {
   topic: "io-cosmosdb-services"
 };
 
-const dummyTargetKafkaConfig = {
+const dummyTargetKafkaConfigWithoutSASL = {
   brokers: ["localhost:9093"],
   clientId: "IO_FUNCTIONS_ELT",
   idempotent: true,
   maxInFlightRequests: 1,
+  ssl: true,
+  transactionalId: "IO_ELT",
+  topic: "io-cosmosdb-services"
+};
+
+const dummyTargetKafkaConfig = {
+  ...dummyTargetKafkaConfigWithoutSASL,
   sasl: {
     mechanism: "plain",
     password: "password",
     username: "username"
-  },
-  ssl: true,
-  transactionalId: "IO_ELT",
-  topic: "io-cosmosdb-services"
+  }
 };
 
 describe("config", () => {
@@ -69,6 +77,18 @@ describe("config", () => {
     expect(E.isRight(aaa)).toBeTruthy();
     if (E.isRight(aaa)) {
       expect(E.getOrElseW(() => "")(aaa)).toStrictEqual(dummyTargetKafkaConfig);
+    }
+  });
+
+  it("GIVEN a valid kafka producer configuration WHEN is without SASL params THEN a right either is returned", () => {
+    const aaa = getKafkaProducerCompactConfigFromEnv("TARGETKAFKA").decode(
+      dummyEnvWithoutSASL
+    );
+    expect(E.isRight(aaa)).toBeTruthy();
+    if (E.isRight(aaa)) {
+      expect(E.getOrElseW(() => "")(aaa)).toStrictEqual(
+        dummyTargetKafkaConfigWithoutSASL
+      );
     }
   });
 });
