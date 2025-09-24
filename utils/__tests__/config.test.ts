f5a4c3a31fed891e10febabab6c9bf7b5c8260e3
arcogabbo <arcogabbo@gmail.com>
2025-09-24 12:40:11 +0200

    [#IOPID-3443] added test user utilities

--- utils/__tests__/config.test.ts
+++ utils/__tests__/config.test.ts
@@ -3,6 +3,8 @@ import {
   getKafkaProducerCompactConfigFromEnv
 } from "../config";
 import * as E from "fp-ts/Either";
+import { TestUsersArrayDecoder } from "../testUser";
+import { gzipSync } from "zlib";
 
 const dummyEnvWithoutSASL = {
   OTHERS: "others env properties",
@@ -71,9 +73,8 @@ describe("config", () => {
   });
 
   it("GIVEN a valid kafka producer configuration WHEN the decode is called THEN a right either is returned", () => {
-    const aaa = getKafkaProducerCompactConfigFromEnv("TARGETKAFKA").decode(
-      dummyEnv
-    );
+    const aaa =
+      getKafkaProducerCompactConfigFromEnv("TARGETKAFKA").decode(dummyEnv);
     expect(E.isRight(aaa)).toBeTruthy();
     if (E.isRight(aaa)) {
       expect(E.getOrElseW(() => "")(aaa)).toStrictEqual(dummyTargetKafkaConfig);
@@ -81,14 +82,27 @@ describe("config", () => {
   });
 
   it("GIVEN a valid kafka producer configuration WHEN is without SASL params THEN a right either is returned", () => {
-    const aaa = getKafkaProducerCompactConfigFromEnv("TARGETKAFKA").decode(
-      dummyEnvWithoutSASL
-    );
+    const aaa =
+      getKafkaProducerCompactConfigFromEnv("TARGETKAFKA").decode(
+        dummyEnvWithoutSASL
+      );
     expect(E.isRight(aaa)).toBeTruthy();
     if (E.isRight(aaa)) {
       expect(E.getOrElseW(() => "")(aaa)).toStrictEqual(
         dummyTargetKafkaConfigWithoutSASL
       );
     }
+  });
+
+  it("GIVEN a valid fiscalcodes list as base64 of a gzipped string WHEN the decode is called THEN a right either is returned", () => {
+    const someFiscalCodes = [
+      "AAAAAA00A00A000B",
+      "AAAAAA00A00A000C",
+      "ISPXNB32R82Y766D"
+    ];
+    const newBase64 = gzipSync(someFiscalCodes.join(",")).toString("base64");
+
+    const res = TestUsersArrayDecoder.decode(newBase64);
+    expect(E.isRight(res)).toBeTruthy();
   });
 });
---
+++ utils/types/__tests__/gzipCompressedString.test.ts
@@ -0,0 +1,59 @@
+import { GzipCompressedString } from "../GzipCompressedString";
+import { gzipSync } from "zlib";
+import * as E from "fp-ts/lib/Either";
+
+describe("GzipCompressedString decoder", () => {
+  const encodeBase64 = (buf: Buffer) => buf.toString("base64");
+
+  it("should decode a correct base64 encoded gzip string", () => {
+    const original = "hello, world!";
+    const gz = gzipSync(original);
+    const b64 = encodeBase64(gz);
+
+    const result = GzipCompressedString.decode(b64);
+    expect(E.isRight(result)).toBe(true);
+    if (E.isRight(result)) {
+      expect(result.right).toBe(original);
+    }
+  });
+
+  it.each`
+    scenario                  | input
+    ${"non base64 string"}    | ${"##@@"}
+    ${"wrong base64 padding"} | ${"ZXhhbXBsZQ="}
+    ${"illegal base64 chars"} | ${"ZXhhbXBsZQ$%"}
+  `("should fail on $scenario", ({ input }) => {
+    const res = GzipCompressedString.decode(input);
+    expect(E.isLeft(res)).toBe(true);
+  });
+
+  it("should fail on valid base64 data but no gzipped", () => {
+    const notGzip = Buffer.from("example", "utf-8");
+    // no gzip
+    const b64 = encodeBase64(notGzip);
+
+    const res = GzipCompressedString.decode(b64);
+    expect(E.isLeft(res)).toBe(true);
+  });
+
+  it.each`
+    scenario               | input
+    ${"empy string"}       | ${""}
+    ${"only space string"} | ${"     "}
+  `("should fail on $scenario", ({ input }) => {
+    const res = GzipCompressedString.decode(input);
+    expect(E.isLeft(res)).toBe(true);
+  });
+
+  it("should support multiline and unicode", () => {
+    const original = "Linea1\nLinea2 — αβγ\nemoji: 😄";
+    const gz = gzipSync(Buffer.from(original, "utf-8"));
+    const b64 = gz.toString("base64");
+
+    const res = GzipCompressedString.decode(b64);
+    expect(E.isRight(res)).toBe(true);
+    if (E.isRight(res)) {
+      expect(res.right).toBe(original);
+    }
+  });
+});
