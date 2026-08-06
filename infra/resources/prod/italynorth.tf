e11c6a4ecad1b7354d95cc3bae5b0c4b75648570
Matteo Anselmi <matteo.anselmi@bitrock.it>
2026-08-06 10:11:16 +0200

    import locals and fix module

--- infra/resources/prod/italynorth.tf
+++ infra/resources/prod/italynorth.tf
@@ -1,19 +1,20 @@
 resource "azurerm_resource_group" "itn_elt" {
-  name     = format("%s-elt-rg-01", local.project_itn)
-  location = var.location_itn
+  name     = format("%s-elt-rg-01", local.project)
+  location = local.location
 
-  tags = var.tags
+  tags = local.tags
 }
 
 module "function_app_elt" {
-  source                          = "../_modules/function_elt"
+  source                          = "../_modules/function_app_elt"
   prefix                          = local.prefix
   env_short                       = local.env_short
-  project_weu_legacy              = local.project_weu_legacy
-  secondary_location_display_name = local.function_elt.secondary_location_display_name
-  location_itn                    = local.function_elt.location_itn
-  vnet_common_name_itn            = local.function_elt.vnet_common_name_itn
-  common_resource_group_name_itn  = local.function_elt.common_resource_group_name_itn
-  elt_snet_cidr                   = local.function_elt.elt_snet_cidr
-  tags                            = local.function_elt.tags
+  project_weu_legacy              = local.project_legacy
+  resource_group_name             = azurerm_resource_group.itn_elt.name
+  secondary_location_display_name = local.secondary_location_display_name
+  location_itn                    = local.location
+  vnet_common_name_itn            = local.vnet_common_name_itn
+  common_resource_group_name_itn  = local.common_resource_group_name_itn
+  elt_snet_cidr                   = local.subnet_cidr
+  tags                            = local.tags
 }
---
+++ infra/resources/prod/locals.tf
@@ -0,0 +1,26 @@
+locals {
+  prefix    = "io"
+  env_short = "p"
+
+  location       = "italynorth"
+  location_short = "itn"
+
+  project        = "${local.prefix}-${local.env_short}-${local.location_short}"
+  project_legacy = "${local.prefix}-${local.env_short}"
+
+  secondary_location_display_name = "North Europe"
+
+  vnet_common_name_itn           = "${local.project}-common-vnet-01"
+  common_resource_group_name_itn = "${local.project}-common-rg-01"
+
+  subnet_cidr = "10.20.40.0/26"
+
+  tags = {
+    CostCenter     = "TS000 - Tecnologia e Servizi"
+    CreatedBy      = "Terraform"
+    Environment    = "Prod"
+    BusinessUnit   = "App IO"
+    Source         = "https://github.com/pagopa/io-functions-elt/blob/main/infra/resources/prod"
+    ManagementTeam = "IO Platform"
+  }
+}
